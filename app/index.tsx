c02b4e2081fb4304be3a81fa6f82a0cf6e079b07
samirjkhadka <findsamirkhadka@gmail.com>
2024-11-20 17:40:13 +0545

    Custom Button completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,20 @@
-import { router } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { TouchableOpacity, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   // return <Redirect href="./(auth)/welcome" />;
 
-  return (
-    <SafeAreaView className="flex h-full items-center justify-between bg-danger-700">
-      <TouchableOpacity
-        className="bg-danger-600"
-        onPress={() => router.replace("./(auth)/welcome")}
-      >
-        <Text>Skip</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+  return <Redirect href="./(auth)/welcome" />
+    // <SafeAreaView className="flex h-full items-center justify-between bg-danger-700">
+    //   <TouchableOpacity
+    //     className="bg-danger-600"
+    //     onPress={() => router.replace("./(auth)/welcome")}
+    //   >
+    //     <Text>Skip</Text>
+    //   </TouchableOpacity>
+    // </SafeAreaView>
+  //);
 };
 
 export default Home;
