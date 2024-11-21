7e73749dba910c4701a9efb12a69442db10ee27a
samirjkhadka <findsamirkhadka@gmail.com>
2024-11-21 14:53:13 +0545

    tailwind preset issue fixed

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import CustomButton from "@/components/CustomButton";
 import { Link } from "expo-router";
 import OAuth from "@/components/oAuth";
+import { useSignUp } from "@clerk/clerk-expo";
 
 const signUp = () => {
   const [form, setForm] = useState({
@@ -13,7 +14,62 @@ const signUp = () => {
     email: "",
     password: "",
   });
-  const onSignUpPress = async () => {};
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [verification, setVerification] = useState({
+    state: "default",
+    error: "",
+    code: "",
+  });
+
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      });
+
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      setVerification({ ...verification, state: "pending" });
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  const onPressVerify = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code: verification.code,
+      });
+
+      if (completeSignUp.status === "complete") {
+        //TODO:create a database user!
+
+        await setActive({ session: completeSignUp.createdSessionId });
+        setVerification({ ...verification, state: "success" });
+      } else {
+        setVerification({
+          ...verification,
+          error: "Verification failed",
+          state: "failed",
+        });
+      }
+    } catch (err: any) {
+      setVerification({
+        ...verification,
+        error: err.errors[0].longMessage,
+        state: "failed",
+      });
+    }
+  };
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -24,49 +80,51 @@ const signUp = () => {
             Create Your Account
           </Text>
         </View>
-      </View>
-      <View className="p-5">
-        <InputField
-          label="Name"
-          icon={icons.person}
-          placeholder="Enter Your Name"
-          value={form.name}
-          onChangeText={(value) => setForm({ ...form, name: value })}
-        />
-        <InputField
-          label="Email"
-          icon={icons.email}
-          placeholder="Enter Your Email"
-          value={form.email}
-          textContentType="emailAddress"
-          onChangeText={(value) => setForm({ ...form, email: value })}
-        />
-        <InputField
-          label="Password"
-          icon={icons.lock}
-          placeholder="Enter Your Password"
-          value={form.password}
-          textContentType="password"
-          secureTextEntry={true}
-          onChangeText={(value) => setForm({ ...form, password: value })}
-        />
-        <CustomButton
-          title="Sign Up"
-          className="mt-6"
-          onPress={onSignUpPress}
-        />
-         {/* oAuth section */}
-        <OAuth />
-        <Link
-          href="/sign-in"
-          className="text-lg text-center text-general-200 mt-10"
-        >
-          <Text className="">Already have an account ? </Text>
-          <Text className="text-primary-500">Login</Text>
-        </Link>
-      </View>
 
-     
+        <View className="p-5">
+          <InputField
+            label="Name"
+            icon={icons.person}
+            placeholder="Enter Your Name"
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            icon={icons.email}
+            placeholder="Enter Your Email"
+            value={form.email}
+            textContentType="emailAddress"
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            icon={icons.lock}
+            placeholder="Enter Your Password"
+            value={form.password}
+            textContentType="password"
+            secureTextEntry={true}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign Up"
+            className="mt-6"
+            onPress={onSignUpPress}
+          />
+          {/* oAuth section */}
+          <OAuth />
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text className="">Already have an account ? </Text>
+            <Text className="text-primary-500">Login</Text>
+          </Link>
+        </View>
+        {/* Verification Modal */}
+
+        
+      </View>
     </ScrollView>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,34 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import '../global.css';
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { LogBox } from "react-native";
 
+import { tokenCache } from "@/lib/auth";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env",
+  );
+}
+
+LogBox.ignoreLogs(["Clerk:"]);
+
+export default function RootLayout() {
   const [loaded] = useFonts({
     "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
     "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
     "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
     "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
     "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
-    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
     "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
   });
 
@@ -34,14 +43,15 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(root)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
